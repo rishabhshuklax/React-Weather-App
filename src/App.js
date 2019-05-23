56b3d06b00cc73864ac0be3b92ae628b3747634a
Firela <rishabhshukla1999@gmail.com>
2019-05-24 00:24:37 +0530

    fix bugs

--- src/App.js
+++ src/App.js
@@ -18,19 +18,23 @@ export default class App extends Component {
     e.preventDefault();
     const city = e.target.elements[0].value;
     const country = e.target.elements[1].value;
-    const dataJSON = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=ad21870045862f95aa7a65818ea7edc7`
-    );
-    const data = await dataJSON.json();
-    console.log(data);
-    this.setState({
-      temperature: Math.floor(data.main.temp - 273.15),
-      city: data.name,
-      country: data.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      req: true
-    });
+    if (city && country) {
+      const dataJSON = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=ad21870045862f95aa7a65818ea7edc7`
+      );
+      const data = await dataJSON.json();
+      console.log(data);
+      data.cod === 200
+        ? this.setState({
+            temperature: Math.floor(data.main.temp - 273.15),
+            city: data.name,
+            country: data.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            req: true
+          })
+        : alert("City Not Found");
+    }
   };
   render() {
     return (
@@ -56,7 +60,7 @@ export default class App extends Component {
               justifyContent: "space-around"
             }}
           >
-            <Form getWeather={this.getWeather} />
+            <Form data={this.state} getWeather={this.getWeather} />
             <Result data={this.state} />
           </div>
         </div>
